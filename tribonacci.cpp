e37cedd231b656980f1aa07e7ede13a65e623f61
oops-aman <b200054@nitsikkim.ac.in>
2023-10-07 13:11:15 +0530

    Tribonacci Number

---
+++ tribonacci.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n <= 1) return n;
+        if(n == 2) return 1;
+        vector<int> fib(n + 1);
+        fib[0] = 0, fib[1] = 1, fib[2] = 1;
+        for(int i = 3; i <= n; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2] + fib[i - 3];
+        }
+        return fib[n];
+    }
+};
